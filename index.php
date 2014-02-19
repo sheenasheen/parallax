f66ad160a15270fb79760a5319afb3243ea7d0d5
sheenasheen <sheena.salmsa@gmail.com>
2014-02-18 23:41:27 -0500

    installed drupal

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
-<html>
-<head><title>Hi</title></head>
-<body>
-	<h1> Hey there 2 </h1>
-</body>
-</html>
+<?php
+
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * The routines here dispatch control to the appropriate handler, which then
+ * prints the appropriate page.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ */
+
+/**
+ * Root directory of Drupal installation.
+ */
+define('DRUPAL_ROOT', getcwd());
+
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+menu_execute_active_handler();
